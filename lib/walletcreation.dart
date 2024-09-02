d52d76bda3a2b97afc509b618d70be6694aa9f23
Ayobami <ayobamiwealth@gmail.com>
2024-09-02 02:22:59 +0100

    update

--- lib/walletcreation.dart
+++ lib/walletcreation.dart
@@ -1,7 +1,3 @@
-import 'dart:convert';
-import 'dart:math';
-
-import 'package:crypto/crypto.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -16,18 +12,13 @@ class WalletCreationPage extends StatefulWidget {
 class _WalletCreationPageState extends State<WalletCreationPage> {
   final TextEditingController _jointPasswordController =
       TextEditingController();
-  // String walletAddress = 'wallet address not available';
-  String? _selectedGroupName;
-  final List<String> _groupNames = [];
-  final List<String> _userNames = [];
-  String walletAddressMessage = '';
+  String walletAddress = 'wallet address not available';
 
   @override
   void initState() {
     super.initState();
     _fetchWalletAddress();
     print(widget.username);
-    checkGroups();
   }
 
   Future<void> _fetchWalletAddress() async {
@@ -38,21 +29,9 @@ class _WalletCreationPageState extends State<WalletCreationPage> {
 
     if (walletDoc.exists) {
       setState(() {
-        walletAddressMessage = walletDoc['walletaddress']?.isNotEmpty == true
+        walletAddress = walletDoc['walletaddress']?.isNotEmpty == true
             ? walletDoc['walletaddress']
-            : walletAddressMessage;
-      });
-    }
-  }
-
-  void checkGroups() async {
-    final groupCollection = FirebaseFirestore.instance.collection('groupinfo');
-    QuerySnapshot groupQuerySnapshot = await groupCollection
-        .where('groupmembers', arrayContains: widget.username)
-        .get();
-    for (var groupDoc in groupQuerySnapshot.docs) {
-      setState(() {
-        _groupNames.add(groupDoc['groupname']);
+            : 'wallet address not available';
       });
     }
   }
@@ -66,152 +45,34 @@ class _WalletCreationPageState extends State<WalletCreationPage> {
       );
       return;
     }
-    // check if the password is ready for the wallet
-    checkForJointPasswordsComplete();
 
     // Set default values if they are not available
     final sharingFormular = '';
     final sharingTime = '';
-    //  final walletAddr =
-    //    walletAddress == walletAddressMessage ? '' : walletAddress;
-    final username = widget.username; // Replace with the actual
-    String verifyUserName = '';
+    final walletAddr =
+        walletAddress == 'wallet address not available' ? '' : walletAddress;
+    final username = 'your-username'; // Replace with the actual username
 
-    final groupWalletCollection =
-        FirebaseFirestore.instance.collection('walletcreationcollection');
-    QuerySnapshot groupQuerySnapshot = await groupWalletCollection
-        .where('username', arrayContains: widget.username)
-        .get();
-    for (var groupWalletDoc in groupQuerySnapshot.docs) {
-      verifyUserName = groupWalletDoc['username'].toString();
-    }
+    try {
+      // Add a new document with the provided data
+      await FirebaseFirestore.instance
+          .collection('walletcreationcollection')
+          .add({
+        'jointpassword': FieldValue.arrayUnion([jointPassword]),
+        'walletaddress': walletAddr,
+        'sharingformular': sharingFormular,
+        'sharingtime': sharingTime,
+        'username': FieldValue.arrayUnion([username]),
+      });
 
-    final collectionSnapshot = await groupWalletCollection.limit(1).get();
-    if (collectionSnapshot.docs.isNotEmpty) {
-      print('Not Empty');
-      print(verifyUserName.contains(username));
-      if (verifyUserName.contains(username)) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('You already created a password')),
-        );
-      } else {
-        // Update the password list with the new password
-        final querySnapshot = await FirebaseFirestore.instance
-            .collection('walletcreationcollection')
-            .where('groupname', isEqualTo: _selectedGroupName)
-            .get();
-
-        for (var doc in querySnapshot.docs) {
-          final groupDocRef = doc.reference;
-          await groupDocRef.update({
-            'jointpassword': FieldValue.arrayUnion([jointPassword]),
-            'username': FieldValue.arrayUnion([username])
-          });
-        }
-
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Wallet data submitted successfully')),
-        );
-      }
-    } else {
-      try {
-        // Add a new document with the provided data
-
-        await FirebaseFirestore.instance
-            .collection('walletcreationcollection')
-            .add({
-          'jointpassword': FieldValue.arrayUnion([jointPassword]),
-          'sharingformular': sharingFormular,
-          'sharingtime': sharingTime,
-          'username': FieldValue.arrayUnion([username]),
-          'groupname': _selectedGroupName
-        });
-
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Wallet data submitted successfully')),
-        );
-      } catch (e) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          // SnackBar(content: Text('Error submitting data: $e')),
-          const SnackBar(
-              content:
-                  Text('Error submitting data, Ensure you belong to a group')),
-        );
-      }
-    }
-  }
-
-  void checkForJointPasswordsComplete() async {
-    // Checking for time to create wallet for the group
-    String numberOfGroupMembers = '';
-    List<dynamic> listOfJointPasswords = [];
-
-    final querySnapshotForNumberOfMembers = await FirebaseFirestore.instance
-        .collection('groupinfo')
-        .where('groupname', isEqualTo: _selectedGroupName)
-        .get();
-    final querySnapshotForNumberOfJointPasswords = await FirebaseFirestore
-        .instance
-        .collection('walletcreationcollection')
-        .where('groupname', isEqualTo: _selectedGroupName)
-        .get();
-
-    for (var groupDoc in querySnapshotForNumberOfJointPasswords.docs) {
-      listOfJointPasswords = groupDoc['jointpassword'];
-    }
-
-    for (var groupDoc in querySnapshotForNumberOfMembers.docs) {
-      numberOfGroupMembers = groupDoc['numberofmembers'].toString();
-    }
-
-    if (listOfJointPasswords.length == int.parse(numberOfGroupMembers)) {
-      createGroupMetaMaskWallet(listOfJointPasswords, _selectedGroupName!);
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Group Wallet successfully created')),
+        const SnackBar(content: Text('Wallet data submitted successfully')),
       );
-    } else {
-      print('Wallet Address not ready, Joint Password Not completed');
-      walletAddressMessage =
-          "Joint Password Not completed, Wallet Address not ready";
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error submitting data: $e')),
+      );
     }
-  }
-
-  String _encryptPassword(String password) {
-    // Convert password to bytes
-    final bytes = utf8.encode(password);
-
-    // Encrypt password using SHA256
-    final digest = sha256.convert(bytes);
-
-    // Convert digest to string
-    return digest.toString();
-  }
-
-  void createGroupMetaMaskWallet(
-      List<dynamic> listOfJointPasswords, String groupName) async {
-    // if this numberOfGroupMembers is equall to the number of jointpasswords create wallet
-    // message your wallet is ready now
-    String randomWalletAddress = generateRandomWalletAddress();
-    final encryptedPassword = _encryptPassword(listOfJointPasswords.join());
-    print('Generated Wallet Address: $randomWalletAddress');
-
-    await FirebaseFirestore.instance
-        .collection('premetaskwalletcollection')
-        .add({
-      'groupname': groupName,
-      'walletaddress': randomWalletAddress,
-      'grouppassword': encryptedPassword,
-    });
-  }
-
-  String generateRandomWalletAddress() {
-    const String chars =
-        '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
-    final Random random = Random.secure();
-    final int addressLength = 34; // Typical length for a Bitcoin address
-
-    return List.generate(
-        addressLength, (index) => chars[random.nextInt(chars.length)]).join();
   }
 
   @override
@@ -237,33 +98,8 @@ class _WalletCreationPageState extends State<WalletCreationPage> {
                   border: Border.all(color: Colors.grey),
                   borderRadius: BorderRadius.circular(4.0),
                 ),
-                child: Text(walletAddressMessage),
+                child: Text(walletAddress),
               ),
-            ),
-            const SizedBox(height: 16.0),
-            DropdownButtonFormField<String>(
-              decoration: const InputDecoration(
-                labelText: 'Group Name',
-                border: OutlineInputBorder(),
-              ),
-              value: _selectedGroupName,
-              items: _groupNames.map((name) {
-                return DropdownMenuItem<String>(
-                  value: name,
-                  child: Text(name),
-                );
-              }).toList(),
-              onChanged: (value) {
-                setState(() {
-                  _selectedGroupName = value;
-                });
-              },
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please select a group';
-                }
-                return null;
-              },
             ),
             const SizedBox(height: 20),
             const Text(
@@ -279,25 +115,9 @@ class _WalletCreationPageState extends State<WalletCreationPage> {
               obscureText: true,
             ),
             const SizedBox(height: 20),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: _submitWalletData,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.zero,
-                  ),
-                  padding: const EdgeInsets.symmetric(vertical: 16),
-                ),
-                child: Text(
-                  'Submit',
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
+            ElevatedButton(
+              onPressed: _submitWalletData,
+              child: const Text('Submit'),
             ),
           ],
         ),
